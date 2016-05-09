f9c6a40a411998969ccf6d3e1e61a87f754ff6ff
fedeoo <xt_devil@sina.com>
2016-05-09 19:24:24 +0800

    update preview global

--- src/skeleton/preview/Preview.jsx
+++ src/skeleton/preview/Preview.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Components from '../../components';
+import Components, { Button } from '../../components';
+import * as actions from '../../actions';
+
 import classNames from 'classnames/bind';
 import styles from './preview.scss';
 const cx = classNames.bind(styles);
 
 class Preview extends Component {
+  constructor() {
+    super();
+    this.onExitPreivew = this.onExitPreivew.bind(this);
+  }
+  onExitPreivew() {
+    let { togglePreview } = this.props;
+    togglePreview();
+  }
+
   render () {
     let {
       connectDropTarget,
@@ -19,14 +30,16 @@ class Preview extends Component {
     })
     return (<div className={className}>
       <div className={cx('ds-preview')}>
-      {
-        childComponents.map((item) => {
-          let { ctype, cid, data } = item;
-          let ItemComponent = Components[ctype];
-          return <ItemComponent key={cid} {...data} />;
-        })
-      }
-    </div></div>);
+        <Button className={cx('exit-btn')} type="primary" text="退出" onClick={this.onExitPreivew} />
+        {
+          childComponents.map((item) => {
+            let { ctype, cid, data } = item;
+            let ItemComponent = Components[ctype];
+            return <ItemComponent key={cid} {...data} />;
+          })
+        }
+      </div>
+    </div>);
   }
 }
 
@@ -40,4 +53,4 @@ function mapState ({ cInstances, cmd }, ownProps) {
   return currentComponent.set('childComponents', childComponents).set('isPreview', isPreview).toJS();
 }
 
-export default connect(mapState)(Preview);
+export default connect(mapState, actions)(Preview);
